a2f5076b624f812d76b68d86a1269f5149561b0e
Cristian Robles <cristian210206.jcrd@gmail.com>
2024-05-02 12:08:09 +0200

    Laboratorio Modulo 7

--- src/motor.spec.ts
+++ src/motor.spec.ts
@@ -1,17 +1,28 @@
 import {vi} from "vitest"
 import {partida, estadoPartida} from "./modelo";
-import { condicionGanarOPerder } from "./motor";
+import * as motor from "./motor";
 import { ganarOPerder } from "./ui";
 
 
-describe(ganarOPerder(condicionGanarOPerder()), () => {
+describe("comprobarVictoria", () => {
     it("Deberia devolver Has_Ganado en caso de que la puntuacion sea 7,5", () => {
     // Arrange
     vi.spyOn(partida, "puntuacion", "get").mockReturnValue(7.5)
-    const resultadoEsperado : estadoPartida = "Has_Ganado" 
+    const resultadoEsperado : estadoPartida = "Has_Ganado"
     // Act
-    const resultado : estadoPartida = condicionGanarOPerder()
+    const resultado = motor.condicionGanarOPerder()
     // Assert
     expect(resultado).toBe(resultadoEsperado)
+    })
+
+    it("Deberia devolver ¡ Lo has clavado! ¡Enhorabuena! en caso de que el estado sea Has_Ganado", () => {
+        // Arrange
+        vi.spyOn(motor, "condicionGanarOPerder").mockReturnValue("Has_Ganado")
+        const resultadoEsperado = "¡ Lo has clavado! ¡Enhorabuena!"
+        // Act
+        motor.condicionGanarOPerder()
+        const resultado : string = ganarOPerder(motor.condicionGanarOPerder())
+        // Assert
+        expect(resultado).toBe(resultadoEsperado)
     })
 })
